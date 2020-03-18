7fc694309b468657054dc8a402612263c8329721
bin1165323 <503775882@qq.com>
2020-03-18 17:35:54 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Login from '../components/Login'
+import Main from '../components/Main'
+
+Vue.use(Router);
+
+export default new Router({
+  routers:[
+    {
+      path:'/login',
+      name:'Login',
+      component:Login
+    },
+    {
+      path:'/main',
+      name:'Main',
+      component:Main
+    }
+  ]
+})
